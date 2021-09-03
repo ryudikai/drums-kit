86b57a3056c9c6691ed4d469d30a5f25f91f2878
Raphael Yudi Kai <42776499+ryudikai@users.noreply.github.com>
2021-09-03 00:17:12 -0300

    HTML, CSS and JS upload

---
+++ index.js
@@ -0,0 +1,77 @@
+// Detecting Button Press
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var buttonInnerHTML = this.innerHTML;
+    pickSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+
+
+// Detecting Key Down
+document.addEventListener("keydown", function(event) {
+  pickSound(event.key);
+  buttonAnimation(event.key);
+});
+
+
+// Play sound
+function pickSound(option) {
+
+  switch (option) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    case "l":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "x":
+      var secret = new Audio("sounds/xaropinho-rapaz.mp3");
+      secret.play();
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+  }
+
+}
+
+// Button animation
+function buttonAnimation (currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
